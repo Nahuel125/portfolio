fe7a29a710c90ef12065cce174da3efab39898c9
Nahuel Martinez <jesusnau.el@hotmail.com>
2022-06-29 17:18:43 -0300

    Primer deploy del proyecto portfolio personal

---
+++ script.js
@@ -0,0 +1,35 @@
+const toggleMenuElement = document.getElementById("toggle-menu");
+const mainMenuElement = document.getElementById("main-menu");
+//const mainMenu = document.getElementsByClassName("main-menu-link");
+
+toggleMenuElement.addEventListener("click", () => {
+  mainMenuElement.classList.toggle("main-menu-show");
+});
+
+/*Funciona peero es rara. 
+for (let i = 0; i < mainMenuLink.length; i++) {
+  mainMenuLink[i].addEventListener("click", function () {
+    mainMenuElement.classList.toggle("main-menu-show");
+  });
+} */
+
+mainMenuElement.addEventListener("click", () => {
+  mainMenuElement.classList.remove("main-menu-show");
+});
+
+const typed = new Typed(".typed", {
+  strings: [
+    "<i class= 'textAnimated' >Nahuel Martinez</i>",
+    "<i class='textAnimated'>Desarrollador Web</i>",
+  ],
+  typeSpeed: 75,
+  startDelay: 600,
+  backSpeed: 75,
+  shuffle: false,
+  backDelay: 1500,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: "|",
+  contentType: "html",
+});
